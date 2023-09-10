55aae7fb683fe2e869ca74642aa010776d8cdc06
jhk1m <jasonkm001@gmail.com>
2023-09-10 11:13:30 -0400

    added response handler

--- lambda/index.js
+++ lambda/index.js
@@ -1,26 +1,22 @@
-/* eslint-disable  func-names */
-/* eslint-disable  no-console */
-/* eslint-disable  no-restricted-syntax */
 const Alexa = require('ask-sdk-core');
-const AWS = require('aws-sdk');
-const ddbAdapter = require('ask-sdk-dynamodb-persistence-adapter'); // included in ask-sdk
+const { Configuration, OpenAIApi } = require('openai');
 const i18n = require('i18next');
 const sprintf = require('i18next-sprintf-postprocessor');
-const { Configuration, OpenAIApi } = require('openai');
-const keys = require('./keys');
 const languageStrings = {
   'en': require('./languages/en')
 }
+const keys = require('./keys');
 
-const config = new Configuration({
-    apiKey: keys.OPEN_AI_KEY
-});
+// Validate OpenAI key
+if (!keys.OPEN_AI_KEY) {
+    throw new Error("OpenAI key is missing");
+}
 
+// OpenAI Configuration
+const config = new Configuration({ apiKey: keys.OPEN_AI_KEY });
 const openai = new OpenAIApi(config);
 
 const DOCUMENT_ID = "visual-response";
-
-// work in progress for screen enabled alexa devices
 const datasource = {
     "headlineTemplateData": {
         "type": "object",
@@ -40,7 +36,7 @@ const datasource = {
             "textContent": {
                 "primaryText": {
                     "type": "PlainText",
-                    "text": "texto"
+                    "text": "text"
                 }
             },
             "logoUrl": "import_logo_here.png",
@@ -49,7 +45,6 @@ const datasource = {
     }
 };
 
-// work in progress for screen enabled alexa devices
 const createDirectivePayload = (aplDocumentId, dataSources = {}, tokenId = "documentToken") => {
     return {
         type: "Alexa.Presentation.APL.RenderDocument",
@@ -77,29 +72,57 @@ async function connectChatGPT(question) {
     return response.data.choices[0].text;
 }
 
+function handleAPL(handlerInput) {
+    // Check if the device supports APL (Alexa Presentation Language)
+    if (Alexa.getSupportedInterfaces(handlerInput.requestEnvelope)['Alexa.Presentation.APL']) {
+        // Ensure datasource is properly defined somewhere in your code
+        if (datasource && datasource.headlineTemplateData && datasource.headlineTemplateData.properties && datasource.headlineTemplateData.properties.textContent) {
+            datasource.headlineTemplateData.properties.textContent.primaryText.text = "Welcome to Gen-AI Enhanced Alexa!";
+        } else {
+            console.warn("Datasource is not properly defined. Skipping APL.");
+        }
+        
+        // Assume createDirectivePayload and DOCUMENT_ID are defined elsewhere
+        const aplDirective = createDirectivePayload(DOCUMENT_ID, datasource);
+
+        // Add the RenderDocument directive to the responseBuilder
+        handlerInput.responseBuilder.addDirective(aplDirective);
+    }
+}
+
+// Helper to generate standard responses
+function generateResponse(handlerInput, messageKey, customMessage = null) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    const speakOutput = customMessage || requestAttributes.t(messageKey);
+
+    if (Alexa.getSupportedInterfaces(handlerInput.requestEnvelope)['Alexa.Presentation.APL']) {
+        handleAPL(handlerInput, speakOutput);
+    }
+
+    return handlerInput.responseBuilder
+        .speak(speakOutput)
+        .reprompt(requestAttributes.t('CONTINUE_MESSAGE'))
+        .withShouldEndSession(false)
+        .getResponse();
+}
+
 // launchrequest intent handler
 const LaunchRequest = {
     canHandle(handlerInput) {
+        // Check if the incoming request is a 'LaunchRequest'
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
     },
     handle(handlerInput) {
         console.log("Incoming Request: ", JSON.stringify(handlerInput.requestEnvelope));
         const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
-        const speechOutput = requestAttributes.t('LAUNCH_MESSAGE');
-        const reprompt = requestAttributes.t('LAUNCH_MESSAGE')
         
-        // work in progress for screen enabled alexa devices
         if (Alexa.getSupportedInterfaces(handlerInput.requestEnvelope)['Alexa.Presentation.APL']) {
-            datasource.headlineTemplateData.properties.textContent.primaryText.text = "Welcome to Alexa-hosted ChatGPT!"
-            // generate the APL RenderDocument directive that will be returned from your skill
-            const aplDirective = createDirectivePayload(DOCUMENT_ID, datasource);
-            // add the RenderDocument directive to the responseBuilder
-            handlerInput.responseBuilder.addDirective(aplDirective);
+          handleAPL(handlerInput);
         }
         
         return handlerInput.responseBuilder
-            .speak(speechOutput)
-            .reprompt(reprompt)
+            .speak(requestAttributes.t('LAUNCH_MESSAGE'))
+            .reprompt(requestAttributes.t('LAUNCH_MESSAGE'))
             .getResponse();
     }
 };
@@ -108,7 +131,6 @@ const LaunchRequest = {
 const ChatGptIntent = {
     canHandle(handlerInput) {
         console.log("DEBUG: ChatGptIntent handler");
-        console.log("Incoming Request: ", JSON.stringify(handlerInput.requestEnvelope));
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest' 
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'ChatGptIntent';
     },
@@ -117,52 +139,27 @@ const ChatGptIntent = {
         console.log("DEBUG: REQUEST TYPE ", Alexa.getRequestType(handlerInput.requestEnvelope))
         console.log("DEBUG: INTENT NAME ", Alexa.getIntentName(handlerInput.requestEnvelope))
         const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
+        sessionAttributes.inConversation = true;  // Set this flag when the conversation starts
+        handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
         const question = Alexa.getSlotValue(handlerInput.requestEnvelope, 'question');
         
         try {
             const response = await connectChatGPT(question);
-            if (question && !response) {
+            if (!response) {
                 console.log("[[[[DEBUG: ChatGptIntent: No response from OpenAI", response, " ]]]]")
-                return handlerInput.responseBuilder
-                    .speak(requestAttributes.t('NO_RESPONSE'))
-                    .reprompt(requestAttributes.t('CONTINUE_MESSAGE'))
-                    .withShouldEndSession(false)
-                    .getResponse();
-            } else if (!question && response){
-                console.log("[[[[DEBUG: ChatGptIntent: No question from user: ", question, " ]]]]")
-                return handlerInput.responseBuilder
-                    .speak(requestAttributes.t('NO_QUESTION'))
-                    .reprompt(requestAttributes.t('CONTINUE_MESSAGE'))
-                    .withShouldEndSession(false)
-                    .getResponse();
-            } else if (question && response) {
-                const speakOutput = response;
-                console.log("[[[[DEBUG: ChatGptIntent: Reponse from OpenAI", response, " ]]]]")
-                
-                // work in progress for screen enabled alexa devices
-                if (Alexa.getSupportedInterfaces(handlerInput.requestEnvelope)['Alexa.Presentation.APL']) {
-                    datasource.headlineTemplateData.properties.textContent.primaryText.text = "Welcome to Alexa-hosted ChatGPT!"
-                    // generate the APL RenderDocument directive that will be returned from your skill
-                    const aplDirective = createDirectivePayload(DOCUMENT_ID, datasource);
-                    // add the RenderDocument directive to the responseBuilder
-                    handlerInput.responseBuilder.addDirective(aplDirective);
-                }
-                
-                return handlerInput.responseBuilder
-                    .speak(speakOutput)
-                    .reprompt(requestAttributes.t('CONTINUE_MESSAGE'))
-                    .withShouldEndSession(false)
-                    .getResponse();
+                return generateResponse(handlerInput, 'NO_RESPONSE');
             }
+            if (!question){
+                console.log("[[[[DEBUG: ChatGptIntent: No question from user: ", question, " ]]]]")
+                return generateResponse(handlerInput, 'NO_QUESTION');
+            } 
+            return generateResponse(handlerInput, null, response);
         
         } catch (error) {
             console.error("Error while calling OpenAI API: ", error);
             // Handle error, maybe send a generic response to the user or a prompt to retry
-            return handlerInput.responseBuilder
-                .speak(requestAttributes.t('OPENAI_ERROR_MESSAGE'))
-                .reprompt(requestAttributes.t('CONTINUE_MESSAGE'))
-                .withShouldEndSession(false)
-                .getResponse();
+            return generateResponse(handlerInput, 'OPENAI_ERROR_MESSAGE');
         }
       },
 };
@@ -228,15 +225,25 @@ const FallbackIntent = {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
           && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.FallbackIntent');
     },
-    handle(handlerInput) {
+    async handle(handlerInput) {
         console.log("Incoming Request: ", JSON.stringify(handlerInput.requestEnvelope));
         const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
         const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
-
-        return handlerInput.responseBuilder
-            .speak(requestAttributes.t('FALLBACK_MESSAGE_OUTSIDE_CHAT'))
-            .reprompt(requestAttributes.t('CONTINUE_MESSAGE'))
-            .getResponse();
+        
+        if (sessionAttributes.inConversation) {
+            const userInput = handlerInput.requestEnvelope.request.intent.slots.any.value || requestAttributes.t('FALLBACK_REPROMPT_DURING_CHAT');
+            
+            try {
+                const response = await connectChatGPT(userInput);
+                return generateResponse(handlerInput, null, response);
+            } catch (error) {
+                console.error("Error while calling OpenAI API: ", error);
+                // Handle error, maybe send a generic response to the user or a prompt to retry
+                return generateResponse(handlerInput, 'OPENAI_ERROR_MESSAGE');
+            } 
+        } else {
+            return generateResponse(handlerInput, 'FALLBACK_MESSAGE_OUTSIDE_CHAT');
+        }
     },
 };
 
@@ -270,7 +277,6 @@ const LocalizationInterceptor = {
 };
 
 const skillBuilder = Alexa.SkillBuilders.custom();
-
 exports.handler = skillBuilder
     .addRequestHandlers(
         LaunchRequest,
--- lambda/languages/en.js
+++ lambda/languages/en.js
@@ -1,15 +1,15 @@
 module.exports = {
   translation: {
-    SKILL_NAME: 'alexagpt',
+    SKILL_NAME: 'Gen-AI Enhanced Alexa',
     EXIT_MESSAGE: 'Ciaos!',
-    FALLBACK_MESSAGE_DURING_CHAT: `Sorry, I have no knowledge about that. Please try asking another question?`,
-    FALLBACK_REPROMPT_DURING_CHAT: 'Can you try asking again?',
-    FALLBACK_MESSAGE_OUTSIDE_CHAT: `With some information, I can give recommendations or feedbacks based on the information provided and much more. Would you like that?`,
+    FALLBACK_MESSAGE_DURING_CHAT: `Sorry, I can't understand the command. Could you try again?`,
+    FALLBACK_REPROMPT_DURING_CHAT: `Did you forget to say "Hey GPT or GPT" before the question?`,
+    FALLBACK_MESSAGE_OUTSIDE_CHAT: `You need to start the conversation with "Hey GPT or just GPT"`,
     LAUNCH_MESSAGE: 'Welcome to ChatGPT! You can start a request by saying Hey GPT',
     CONTINUE_MESSAGE: 'Would you like to continue this conversation?',
-    HELP_MESSAGE: 'You can ask something like, what is the Linux command to shut down the PC?',
+    HELP_MESSAGE: 'You can ask something like, "Hey GPT, what is the Linux command to shut down the PC?"',
     HELP_REPROMPT: 'Would you like to try another question?',
-    ERROR_MESSAGE: 'Sorry, an error occurred.',
+    ERROR_MESSAGE: 'Sorry, something went wrong',
     OPENAI_ERROR_MESSAGE: 'Sorry, I could not connect to OpenAI using provided credentials',
     INFO_MESSAGE: 'I need more information to continue'
   }
